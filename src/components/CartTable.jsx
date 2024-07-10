8c19234890a4e0a0eedfb41628d4581a32a80a32
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-07-10 16:59:15 +0300

    Modified table design to include
    Modified table design to include:
    - Item quantity
    - Item pricing
    - Styling modifications

--- src/components/CartTable.jsx
+++ src/components/CartTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBListGroup, MDBListGroupItem, MDBBadge } from 'mdb-react-ui-kit';
+import { MDBListGroup, MDBListGroupItem, MDBBadge, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const CartTable = () => {
@@ -15,30 +15,70 @@ const CartTable = () => {
                 <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
                     <div className="d-flex">
                         <img src="images/jacket.jpg" class="rounded float-start" height="100"></img>
-                        <div className="div flex-column p-3">
+                        <div className="div flex-column p-3 ms-3">
                             <div className='text-start fw-bold'>John Doe</div>
                             <div className='text-muted'>john.doe@gmail.com</div>
                         </div>
                     </div>
+                    <div className="d-flex">
+                        <div className="div p-2">
+                            <MDBDropdown>
+                                <MDBDropdownToggle color='light' >1</MDBDropdownToggle>
+                                <MDBDropdownMenu>
+                                    <MDBDropdownItem link>1</MDBDropdownItem>
+                                    <MDBDropdownItem link>2</MDBDropdownItem>
+                                    <MDBDropdownItem link>3</MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </div>
+
+                        <div className="div flex-column ms-3">
+                            <div className='text-start fw-bold'>$460.00</div>
+                            <div className='text-muted'>$460.00 / per item</div>
+                        </div>
+                    </div>
+
                     <MDBBadge pill light color='success'>
                         Active
                     </MDBBadge>
                 </MDBListGroupItem>
 
                 <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-                    <div>
-                        <div className='fw-bold'>Alex Ray</div>
-                        <div className='text-muted'>alex.ray@gmail.com</div>
+                    <div className="d-flex">
+                        <img src="images/jacket.jpg" class="rounded float-start" height="100"></img>
+                        <div className="div flex-column p-3 ms-3">
+                            <div className='text-start fw-bold'>John Doe</div>
+                            <div className='text-muted'>john.doe@gmail.com</div>
+                        </div>
                     </div>
+                    <MDBDropdown>
+                        <MDBDropdownToggle color='light'>1</MDBDropdownToggle>
+                        <MDBDropdownMenu>
+                            <MDBDropdownItem link>1</MDBDropdownItem>
+                            <MDBDropdownItem link>2</MDBDropdownItem>
+                            <MDBDropdownItem link>3</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
                     <MDBBadge pill light color='primary'>
                         Onboarding
                     </MDBBadge>
                 </MDBListGroupItem>
                 <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-                    <div>
-                        <div className='fw-bold'>Kate Hunington</div>
-                        <div className='text-muted'>kate.hunington@gmail.com</div>
+                    <div className="d-flex">
+                        <img src="images/jacket.jpg" class="rounded float-start" height="100"></img>
+                        <div className="div flex-column p-3 ms-3">
+                            <div className='text-start fw-bold'>John Doe</div>
+                            <div className='text-muted'>john.doe@gmail.com</div>
+                        </div>
                     </div>
+                    <MDBDropdown>
+                        <MDBDropdownToggle color='light'>1</MDBDropdownToggle>
+                        <MDBDropdownMenu>
+                            <MDBDropdownItem link>1</MDBDropdownItem>
+                            <MDBDropdownItem link>2</MDBDropdownItem>
+                            <MDBDropdownItem link>3</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
                     <MDBBadge pill light color='warning'>
                         Awaiting
                     </MDBBadge>
